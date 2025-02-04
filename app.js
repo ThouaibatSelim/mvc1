32b5ae22e62b881acb0ed2f842c28c4ecd20ceb6
ThouaibatSelim <selimthouaibat9@gmail.com>
2025-02-04 17:19:49 +0300

    4/2

--- app.js
+++ app.js
@@ -6,11 +6,56 @@ const myConnection = require("express-myconnection");
 
 const app = express();
 
+const connection = {
+    host : 'localhost',
+    user : 'root',
+    password : 'mythouaiL9222',
+    port : 3005,
+    database : 'chainetv'
+};
+
+//Vues
+app.set("views", "./views"); //emplacement
+app.set("view engine", "ejs"); //moteur
+
+app.use(express.static("public"));
+
 // accueil
 app.get("/accueil", (req, res) => {
     //renvoyer la page accueil
         res.render("accueil");
     });
+
+//programme tv
+app.get("/ptv", (req, res) => {
+    //renvoyer la page accueil
+        res.render("ptv");
+    });
+
+//recrutement
+app.get("/recrutement", (req, res) => {
+    //renvoyer la page accueil
+        res.render("recrutement");
+    });
+
+//recrutement
+app.get("/direct", (req, res) => {
+    //renvoyer la page accueil
+        res.render("direct");
+    }); 
+
+//nouveautés
+app.get("/nouveautes", (req, res) => {
+    //renvoyer la page accueil
+        res.render("nouveautes");
+    });
+
+//formulaire programme tv
+app.get("/formptv", (req, res) => {
+    //renvoyer la page accueil
+        res.render("formptv");
+    });
+
 
 //A propos
 app.get("/apropos", (req, res) => {
@@ -29,3 +74,5 @@ app.get("/inscription", (req, res) => {
     //renvoyer la page inscription
         res.render("inscription");
     });
+
+    module.exports = app;
